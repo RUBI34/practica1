77bc11b2cb2989c84af6a5868fb91821af6999d5
unknown <mmarque697@gmail.com>
2021-02-27 14:52:57 -0600

    firts commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function suma(){
+    let num1;
+    let num1;
+    let res;
+    num1=document.getElementById("num1").value;
+    num2=document.getElementById("num2").value;
+    res=parseInt(num1) + parseInt(num2);
+    document.getElementById("res").innerHTML = res;
+}
